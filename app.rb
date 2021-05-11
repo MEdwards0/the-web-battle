d307080daf256c5e5863aa27253447a15c015d47
Matthew <matt.edwards21@protonmail.com>
2021-05-11 11:40:11 +0100

    Adding web pages with cat erb index

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  'good morning/afternoon/evening'
+end
+
+get '/secret' do
+  'shh i am a banana'
+end
+
+get '/banana' do
+  'There are no bananas here.'
+end
+
+get '/recipe' do
+  "<h1>Testing</h1>
+
+  <ul>
+    <li>banana</li>
+    <li>two bananas</li>
+    <li>three bananas</li>
+  </ul>"
+end
+
+get '/cat' do
+  erb(:index) # can be named anything
+end
