b8768fd05228ea5ebb842a482132cb3249155fd9
ekjones1993 <emilykjones1993@gmail.com>
2021-05-12 16:24:59 +0100

    created sessions, added hp

--- battle/spec/features/enter_names_spec.rb
+++ battle/spec/features/enter_names_spec.rb
@@ -1,11 +1,11 @@
 feature 'enter names' do
   scenario 'submit names' do
-    visit('/') 
+    visit('/')
     fill_in :player_1_name, with: 'Regina'
     fill_in :player_2_name, with: 'Balthazar'
     click_button 'Submit'
 
-    save_and_open_page
+    #save_and_open_page
 
     expect(page).to have_content 'Regina vs. Balthazar'
   end
