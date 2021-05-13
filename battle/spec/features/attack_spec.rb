5b3da90cc645a0b3ce0b666a2b95fd826ba8e0e0
Adam R <adamroyal12@gmail.com>
2021-05-13 12:37:39 +0100

    people be losing HP

--- battle/spec/features/attack_spec.rb
+++ battle/spec/features/attack_spec.rb
@@ -4,4 +4,10 @@ feature 'attacking' do
     click_link 'Attack'
     expect(page).to have_content "Regina attacked Balthazar!"
   end
+  scenario 'reduce player 2 hitpoints by 10' do
+    sign_in_and_play
+    click_link 'Attack'
+    expect(page).not_to have_content 'Balthazar: 1000HP'
+    expect(page).to have_content 'Balthazar: 990HP'
+  end
 end
---
+++ battle/spec/player_spec.rb
@@ -0,0 +1,31 @@
+require 'player'
+
+describe Player do
+  subject(:regina) { Player.new('Regina') }
+  subject(:balthazar) { Player.new('Balthazar') }
+
+  describe '#name' do
+    it 'returns the name' do
+      expect(regina.name).to eq 'Regina'
+    end
+  end
+
+  describe '#hitpoints' do
+    it 'returns the hit points' do
+      expect(regina.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+  end
+
+  describe '#attack' do
+    it 'damages the player' do
+      expect(balthazar).to receive(:receive_damage)
+      regina.attack(balthazar)
+    end
+  end
+
+  describe '#receive_damage' do
+    it 'reduces the player hit points' do
+      expect { regina.receive_damage }.to change { regina.hit_points }.by(-10)
+    end
+  end
+end
